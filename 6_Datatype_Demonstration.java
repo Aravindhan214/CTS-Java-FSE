91d3786715710a4c1a45bfb2fa7884af1b67e276
Aravindhan214 <aravindhanaravindhan69@gmail.com>
2025-05-22 18:55:36 +0530

    Create 6_Datatype_Demonstration.java

---
+++ 6_Datatype_Demonstration.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Datatype {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 3.14f;
+        double c = 123.456;
+        char d = 'A';
+        boolean e = true;
+
+        // Displaying the values using System.out.println()
+        System.out.println("Integer value:   " + a);
+        System.out.println("Float value:     " + b);
+        System.out.println("Double value:    " + c);
+        System.out.println("Character value: " + d);
+        System.out.println("Boolean value:   " + e);
+    }
+} 
